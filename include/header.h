99ed423d4213cd061f8ecf047cdf2f01813170de
DinoMalin <juliancario@orange.fr>
2024-06-10 14:42:53 +0200

    + | Added parsing to differenciate args

--- include/header.h
+++ include/header.h
@@ -1,1 +1,28 @@
 #include "libft.h"
+#include <stdlib.h>
+
+typedef enum {
+	OPTION		= 1 << 0,
+	LONG_OPTION	= 1 << 1,
+	ARG			= 1 << 2,
+	FILE		= 1 << 3,
+	FOLDER		= 1 << 4
+} Arg_type;
+
+typedef enum {
+	list		= 1 << 0,
+	recursive	= 1 << 1,
+	all			= 1 << 2,
+	reverse		= 1 << 3,
+	time		= 1 << 4
+} Flag;
+
+typedef struct {
+	Arg_type	type;
+	char		*content;
+} Arg;
+
+typedef struct {
+	Arg *args;
+	Flag flags;
+} Command;
--- src/main/main.c
+++ src/main/main.c
@@ -1,5 +1,38 @@
-#include <header.h>
+#include "header.h"
 
-int main() {
-	ft_printf("I'm a dino!");
+void get_arg(Arg *curr, char *str) {
+	if (str[0] == '-') {
+		curr->type = OPTION;
+		if (str[1] == '-') {
+			curr->type |= LONG_OPTION;
+			curr->content = ft_strdup(str + 2);
+		} else
+			curr->content = ft_strdup(str + 1);
+	} else {
+		curr->type = ARG;
+		curr->content = ft_strdup(str);
+	}
+}
+
+Arg *parse_args(int ac, char **av) {
+	Arg	*result;
+
+	result = malloc((ac - 1) * sizeof(Arg));
+	for (int i = 1; i < ac; i++) {
+		get_arg(&result[i - 1], av[i]);
+	}
+	return result;
+}
+
+int main(int ac, char **av) {
+	Arg *args = parse_args(ac, av);
+
+	for (int i = 0; i < ac - 1; i++) {	
+		if (args[i].type & OPTION)
+			ft_printf("OPTION (%s)\n%s\n\n", args[i].type & LONG_OPTION ? "LONG" : "SIMPLE", args[i].content);
+		else if (args[i].type & ARG)
+			ft_printf("ARG\n%s\n\n", args[i].content);
+		else
+			ft_printf("I have no idea what this thing is");	
+	}
 }
